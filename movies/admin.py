fd90e272d3fa2a641358c6f0e428e11cfa757291
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2025-09-07 11:04:07 +0330

    feat: create movie admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'user', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('release_date', 'created_at', 'user')
+    ordering = ('-release_date', '-created_at')
+    readonly_fields = ('created_at', 'updated_at')
