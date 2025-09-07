b4502a02ea7135a50ffb20a44c39fcfd532f43d3
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2025-09-07 11:01:42 +0330

    feat: create movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    # == Fields ==
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    release_date = models.DateField()
+    media = models.FileField(upload_to='movies/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # == Relations ==
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='movies')
+
+    def __str__(self):
+        return self.title
