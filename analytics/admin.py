ade76b837848b824cb83b57dbd09e3aba13461c5
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2025-09-07 11:42:19 +0330

    feat: create rate model and admin

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rate
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ('score', 'movie', 'user', 'created_at')
+    search_fields = ('movie__title', 'user__username')
+    list_filter = ('score', 'created_at', 'updated_at')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Rate(models.Model):
+    # == Fields ==
+    score = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # == Relations ==
+    movie = models.ForeignKey('movies.Movie', on_delete=models.CASCADE, related_name='rates')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='rates')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['movie', 'user'], name='unique_movie_user_rate')
+        ]
+
+    def __str__(self):
+        return f'Rate {self.score} for Movie<{self.movie_id}> by User<{self.user_id}>'
